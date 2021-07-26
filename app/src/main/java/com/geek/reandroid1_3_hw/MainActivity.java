2375ceb39274ffd3c31a70aecd80a251a61ea724
JKramer07 <turanraiga@rocketmail.com>
2021-07-26 16:49:35 +0600

    added

--- app/src/main/java/com/geek/reandroid1_3_hw/MainActivity.java
+++ app/src/main/java/com/geek/reandroid1_3_hw/MainActivity.java
@@ -34,6 +34,19 @@ public class MainActivity extends AppCompatActivity {
         countries.add(new Countries("Ukraine", "Kiev", R.drawable.ua));
         countries.add(new Countries("Kazakhstan", "Astana", R.drawable.kz));
         countries.add(new Countries("USA", "Washington", R.drawable.usa));
+        countries.add(new Countries("Great Britain", "London", R.drawable.britain));
+        countries.add(new Countries("Canada", "Ottawa", R.drawable.canada));
+        countries.add(new Countries("Germany", "Berlin", R.drawable.germany));
+        countries.add(new Countries("India", "New-Deli", R.drawable.india));
+        countries.add(new Countries("Israel", "Tel-Aviv", R.drawable.israel));
+        countries.add(new Countries("Italy", "Rome", R.drawable.italy));
+        countries.add(new Countries("Japan", "Tokyo", R.drawable.japan));
+        countries.add(new Countries("Mexico", "Mexico", R.drawable.mexico));
+        countries.add(new Countries("Poland", "Warsaw", R.drawable.poland));
+        countries.add(new Countries("Portugal", "Lisbon", R.drawable.portugal));
+        countries.add(new Countries("Sweden", "Stockholm", R.drawable.sweden));
+        countries.add(new Countries("Turkey", "Ankara", R.drawable.turkey));
+        countries.add(new Countries("Uzbekistan", "Tashkent", R.drawable.uzbekistan));
         return countries;
     }
 }
